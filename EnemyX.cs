1091a498ab9fbd78e4f207ea56e3e789418a37b4
Ivanhern <103946106+Ivanhern@users.noreply.github.com>
2022-04-18 16:57:33 +0200

    Update EnemyX.cs

--- EnemyX.cs
+++ EnemyX.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyX : MonoBehaviour
+public class EnemyX : MonoBehaviou
 {
     public float speed;
     private Rigidbody enemyRb;
